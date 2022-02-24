192ec9f6ec6cdeed7d3a23232d9cfba1e2aa36ef
anyu-yu <andrew.yu.x@gmail.com>
2022-02-24 10:53:32 -0800

    finished 2-4

--- sql/01.sql
+++ sql/01.sql
@@ -1,3 +1,14 @@
 /*
  * Compute the number of customers who live outside of the US.
  */
+
+SELECT COUNT(*) AS count
+FROM customer
+WHERE customer_id IN (
+    SELECT DISTINCT customer.customer_id
+    FROM customer
+    JOIN address USING (address_id)
+    JOIN city USING (city_id)
+    JOIN country USING (country_id)
+    WHERE NOT(country.country = 'US')
+);
--- sql/02.sql
+++ sql/02.sql
@@ -1,3 +1,12 @@
 /*
  * Compute the country with the most customers in it. 
  */
+
+SELECT country.country
+FROM country
+JOIN city USING (country_id)
+JOIN address USING (city_id)
+JOIN customer USING (address_id)
+GROUP BY country.country
+ORDER BY COUNT(*) DESC
+LIMIT 1;
